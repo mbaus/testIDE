fee3094b2151a64e41a348dd125eb008d88ca7ea
Martin Baus <mb@dacomb.info>
2018-04-17 22:50:31 +0200

    initial commit

---
+++ BatchHonkLib/Dummy.java
@@ -0,0 +1,23 @@
+==========++++++++++==========
+./BatchHonkLib/Dummy.java
+package de.honk.dummy;
+
+import javax.ejb.LocalBean;
+import javax.ejb.Stateless;
+
+/**
+ * Session Bean implementation class Dummy
+ */
+@Stateless(mappedName = "dummy")
+@LocalBean
+public class Dummy {
+
+    /**
+     * Default constructor. 
+     */
+    public Dummy() {
+        // TODO Auto-generated constructor stub
+    }
+
+}
+
